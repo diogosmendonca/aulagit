c2a2e353e174c14a73c9a811f5023f6572623aff
DIOGO MENDONCA <09513592740@cefet-rj.br>
2018-08-01 19:54:45 -0300

    Alterção do hello world

--- HelloJava/src/hellojava/HelloJava.java
+++ HelloJava/src/hellojava/HelloJava.java
@@ -16,7 +16,9 @@ public class HelloJava {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!");
+        
+        System.out.println("Aula de PSW!!!");
         
     }
     
