fd35e09a48b229ae2426046a07c7e160f53ebe1d
DIOGO MENDONCA <diogosmendonca@gmail.com>
2018-08-08 19:30:09 -0300

    Exemplo de logs

--- mavenproject1/src/main/java/br/cefetrj/mavenproject1/HelloWorld.java
+++ mavenproject1/src/main/java/br/cefetrj/mavenproject1/HelloWorld.java
@@ -5,14 +5,38 @@
  */
 package br.cefetrj.mavenproject1;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author 09513592740
  */
 public class HelloWorld {
     
+    static Logger lg = Logger.getLogger(HelloWorld.class);
+    
+    public static int soma(int a, int b){
+        lg.trace("soma " + a + " + " + b);
+        int r = a - b; 
+        lg.trace("resultado da soma = " + r);
+        return r;
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        
+        lg.info("main chamado");
+        try{
+            if(args.length == 0)
+                lg.warn("Programa executado sem nenhum parâmetro");
+            
+            System.out.println("Soma = " + soma(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+        }catch(Exception e){
+            lg.error("Erro ao executar o hello world!!!", e);
+            lg.fatal(e);
+        }
+        lg.debug("DEBUG");
+        lg.trace("TRACE");
+        lg.info("terminou main");
     }
     
 }
